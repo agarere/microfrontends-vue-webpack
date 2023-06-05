c051afebf2a9a03133b98e6501703a6c15e04469
Ediz Agarer <ediz.agarer@teamdefinex.com>
2023-06-05 11:39:56 +0300

    Fixed remote url

--- vue-dashboard/vue.config.js
+++ vue-dashboard/vue.config.js
@@ -1,13 +1,14 @@
 const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
+  publicPath: "http://localhost:8081",
   chainWebpack: config => {
     config.optimization.delete("splitChunks");
     config
       .plugin("module-federation-plugin")
       .use(require("webpack").container.ModuleFederationPlugin, [{
         remotes: {
-          "notifications": "notifications@/notifications/remoteEntry.js"
+          "notifications": "notifications@http://localhost:8080/remoteEntry.js"
         },
         shared: {
           "vue": {
